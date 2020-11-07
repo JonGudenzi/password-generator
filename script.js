b11472a62290039b44d75a1b8ca3e752d514904b
JonGudenzi <jongudenzi@gmail.com>
2020-11-06 21:17:51 -0500

    Added the question confirms to the user and made them with the confirm variables. The users answers will be stored in these variables.

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function writePassword()
-
-
 // confirming (option) variables
 var howMany;
 var confirmNumber;
@@ -73,10 +55,16 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+////////////////////////////////////////////////////////////////////////////////////
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
-function writePassword()
-////////////////////////////////////////////////////////////////////////////////////
+// function writePassword()
+
+// the confirm variables/options. The users responses will be stored here.
+confirmNumber = confirm("Do you want the password to have numbers?");
+confirmCharacter = confirm("Do you want the password to have characters?");
+confirmUppercase = confirm("Do you want the password to have uppercase letters?");
+confirmLowercase = confirm("Do you want the password to have lowercase letters?");
+   
